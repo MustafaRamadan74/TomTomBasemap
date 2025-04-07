bd1d33983c39a0dbe8c9d72faf340c224baae950
Mostafa Ramadan <mramadan@GeoPro.local>
2025-04-07 11:52:32 +0200

    sec

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
 // 31.3508916099721,30.057316244023994
-// let center = [31.3508916099721,30.057316244023994]
-let center = [39.946714,21.554412]
+let Geopro = [31.3508916099721,30.057316244023994]
+
 const map = tt.map({
     key: "NkYMUbTzRUoHALzkBZHf9PdS3KA5VEWg",
     container: "map",
-    center: center,
+    center: Geopro,
     zoom: 14
 })
 map.on("load",() => {
-    new tt.Marker().setLngLat(center).addTo(map)
+    new tt.Marker().setLngLat(Geopro).addTo(map)
 })
 
-// Traffic Flow Tiles Tier
-
-// var styleBase = "tomtom://vector/1/"
-// var styleS1 = "s1"
-// var styleRelative = "relative"
-// var refreshTimeInMillis = 30000
-
-// var trafficFlowTilesTier = new tt.TrafficFlowTilesTier({
-//   key: "NkYMUbTzRUoHALzkBZHf9PdS3KA5VEWg",
-//   style: styleBase + styleRelative,
-//   refresh: refreshTimeInMillis,
-// })
-
-// function toggleTrafficFlowTilesTier() {
-//   if (document.getElementById("flow-toggle").checked) {
-//     map.addTier(trafficFlowTilesTier)
-//   } else {
-//     map.removeTier(trafficFlowTilesTier.getId())
-//   }
-// }
-// document.getElementById("flow-toggle").addEventListener("change", toggleTrafficFlowTilesTier)
